4f8b7730f69299d820a50bd831edb929971093b4
manishverma0000 <manishverma88180@gmail.com>
2026-03-15 19:16:59 +0530

    updated the connection of mongodb

--- app/db/session.py
+++ app/db/session.py
@@ -1,5 +1,9 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from app.core.config import settings
+from dotenv import load_dotenv
+import random;
+import os
+load_dotenv()
 
 class Database:
     client: AsyncIOMotorClient = None
@@ -10,7 +14,7 @@ def get_db():
     return db.client[settings.DATABASE_NAME]
 
 async def connect_to_mongo():
-    db.client = AsyncIOMotorClient(settings.MONGODB_URL)
+    db.client = AsyncIOMotorClient(os.getenv("MONGODB_URL"))
 
 async def close_mongo_connection():
     if db.client:
