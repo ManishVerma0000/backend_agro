b06338f9f19e999d257fa499eb3f82ec71a486d6
manishverma0000 <manishverma88180@gmail.com>
2026-03-15 12:43:08 +0530

    added the cors for the frontend

--- app/main.py
+++ app/main.py
@@ -24,7 +24,9 @@ app = FastAPI(
 origins = [
     "http://localhost",
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://agro-hub-dun.vercel.app/",
+    "*"
 ]
 
 app.add_middleware(
